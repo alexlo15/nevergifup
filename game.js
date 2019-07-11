85d44919a039cab17c40eb74438499fe22633b77
Alex Lo <loalexander389@gmail.com>
2019-07-11 16:41:28 -0400

    css js changes

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
-
+// on page load
 $(document).ready(function () {
-
+// our topics or teams
     var topics = ["New York Knicks", "Brooklyn Nets", "Utah Jazz", "Golden State Warriors", "Toronto Raptors", "Oklahoma City Thunder", "Los Angeles Lakers", "Philadelphia 76ers", "Miami Heat", "Portland Trailblazers"]
     
+// where we will store our gifs
     gifshere = " "
 
-    function makeButtons() {
+    // this function will make the buttons for each item in the array
+    // it will add classes, text, attr and push them to html
+    var makeButtons= function() {
 
         $("#buttons").empty();
 
@@ -22,8 +25,8 @@ $(document).ready(function () {
 
             $("#buttons").append(a);
 
-        }
 
+        }
         $("#team-input").focus();
 
     }
@@ -32,45 +35,36 @@ $(document).ready(function () {
     makeButtons();
 
 
-    $("#add-team").on('click', function () {
 
-
-        event.preventDefault();
-
-        var team = $("#team-input").val().trim();
-
-        topics.push(team);
-
-        makeButtons();
-
-    });
-
+    // on click we'll empty our gifs, load the new team, then tap into our API
+    
     $(document).on('click', 'button', function () {
 
         $('#gifshere').empty();
 
-        var b = $(this).attr('data-name');
+        var gifteam = $(this).attr('data-name');
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=JWgyHr2Qwkv09gHZn9tsUdBz3ca18dEA&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifteam + "&api_key=JWgyHr2Qwkv09gHZn9tsUdBz3ca18dEA&limit=10";
 
         $.ajax({
             url: queryURL,
             method: 'GET'
-        }).done(function (response) {
-
+        }).then(function (response) {
+            // after they get back, we'll create a div img for each,
+            // the limit is set to 10 in the apikey
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
 
-                var gifDiv = $('<div class="item">');
+                var gifDiv = $('<div class="teamgif">');
 
                 var gifPic = $('<img>');
 
-                gifPic.attr('src', results[i].images.fixed_height_still.url)
+                gifPic.attr('src', results[i].images.fixed_height.url)
                     .attr('data-still', results[i].images.fixed_height_still.url)
                     .attr('data-animate', results[i].images.fixed_height.url)
                     .attr('data-state', "still")
-                    .addClass("showImage");
+                    .addClass("active");
 
             
                 gifDiv.append(gifPic);
@@ -81,8 +75,57 @@ $(document).ready(function () {
         });
     });
 
+    // when addteam is clicked we will push their input to topics
+// then run the make button function again to make the newly populated
+// array into buttons
 
-    $(document).on('click', '.showImage', function () {
+$("#add-team").on('click', function () {
+
+
+    event.preventDefault();
+
+    var team = $("#team-input").val().trim();
+
+    topics.push(team);
+
+    makeButtons();
+    //  $('#gifshere').empty();
+
+    //     var gifteam = $(this).attr('data-name');
+
+    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifteam + "&api_key=JWgyHr2Qwkv09gHZn9tsUdBz3ca18dEA&limit=10";
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: 'GET'
+    //     }).then(function (response) {
+    //         // after they get back, we'll create a div img for each,
+    //         // the limit is set to 10 in the apikey
+    //         var results = response.data;
+
+    //         for (var i = 0; i < results.length; i++) {
+
+    //             var gifDiv = $('<div class="teamgif">');
+
+    //             var gifPic = $('<img>');
+
+    //             gifPic.attr('src', results[i].images.fixed_height.url)
+    //                 .attr('data-still', results[i].images.fixed_height_still.url)
+    //                 .attr('data-animate', results[i].images.fixed_height.url)
+    //                 .attr('data-state', "still")
+    //                 .addClass("active");
+
+            
+    //             gifDiv.append(gifPic);
+
+    //             $('#gifshere').prepend(gifDiv);
+    //         }
+
+    //     });
+
+});
+    
+    $(document).on('click', '.active', function () {
 
         var state = $(this).data('state');
 
